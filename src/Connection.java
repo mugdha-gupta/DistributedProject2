32d933c1ff96c3afc13f6c054270dd66790fa089
mugdha-gupta <mxg167030@utdallas.edu>
2019-11-05 18:17:49 -0600

    connection

--- src/Connection.java
+++ src/Connection.java
@@ -1,8 +1,29 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Connection {
-    public final int ID_UNKNOWN = -1;
+    private HashMap<Integer, Queue<Message>> input;
+    private HashMap<Integer, Queue<Message>> output;
+    public Connection(int processId1, int processId2){
+        Queue<Message> queue1 = new LinkedList<>();
+        input.put(processId1, queue1);
+        output.put(processId2, queue1);
 
-    
-    public Connection(){
+        Queue<Message> queue2 = new LinkedList<>();
+        input.put(processId2, queue2);
+        output.put(processId1, queue2);
+    }
 
+    public void sendMessage(int myId, Message message){
+        output.get(myId).add(message);
+    }
+
+    public Message getMessage(int myId){
+        return input.get(myId).poll();
     }
 }
