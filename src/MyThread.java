ca7d4a1194a16b1882b0e96027f414df9d5a9df0
nymishaj <nymz47@gmail.com>
2019-11-05 18:45:35 -0600

    working on processing messages

--- src/MyThread.java
+++ src/MyThread.java
@@ -1,29 +1,34 @@
 import java.io.IOException;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
 
+
+
 class MyThread extends Thread {
+
+    public final int INIT = 0;
+    public final int ACCEPT = 1;
+    public final int DECLINE = 2;
 
 
     public int id;
     public int maxIdFound;
-    int parent;
-    ArrayList<Integer> neighbors;
-    int responseCounter = 0;
+    public int parent;
+    public ArrayList<Integer> neighbors;
+    public Queue<Message> recievedMessages;
+    public int responseCounter = 0;
 
     public MyThread(int my_id, ArrayList<Integer> my_neighbors) {
-        //initialize our class variables
 
         id = my_id;
         maxIdFound = my_id;
         parent = -1;
         neighbors = my_neighbors;
+        recievedMessages = new LinkedList<>();
 
 
         //start from within constructor so main thread never has to call it
@@ -39,14 +44,56 @@ class MyThread extends Thread {
 
     public void initialize(){
         System.out.println("initializing " + this.id);
+        sendMessages(new Message(id, id, INIT));
 
     }
     public void receiveMessages(){
         System.out.println("receiving " + this.id);
 
+
     }
     public void processMessages(){
         System.out.println("processing "+ this.id);
+        while (!recievedMessages.isEmpty())
+        {
+            Message message = recievedMessages.poll();
+            if (message.type == INIT) {
+                if (maxIdFound < message.maxIdFound){
+                    parent = message.senderid;
+                    sendMessages(new Message(id, parent, INIT));
+                }
+                else {
+                    sendResponse(new Message(id, maxIdFound, DECLINE), message.senderid);
+
+                }
+
+            }
+            else if (message.type == ACCEPT){
+                responseCounter++;
+
+            }
+            else if (message.type == DECLINE){
+                responseCounter++;
+//                if (maxIdFound == message.maxIdFound) {
+//                    responseCounter++;
+//                }
+
+            }
+            else{
+                System.out.println("invalid type of message");
+            }
+
+        }
+
+    }
+
+    public void sendMessages(Message message){
+        System.out.println("send messages to neighbors here");
+
+    }
+
+    public void sendResponse(Message message, int parent){
+        System.out.println("send response to potential parent here");
 
     }
 
